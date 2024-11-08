2f7b4362ce3ec97789630bc37c7c72b4e4f3d368
Armand Lecourt <armandlecourt2003@gmail.com>
2024-11-08 12:05:50 +0100

    Server

--- src/Server.java
+++ src/Server.java
@@ -1,28 +1,41 @@
 // Socket sur lequel le serveur attends les connexions
-public class Server {
-    
-    try{
-        serverSocket = new ServerSocket(4444);
-    }
-    catch(IOException e){
-        System.out.println("Could not listen on port 4444");
-        System.exit(-1);
-    }
 
-    Socket clientSocket = null;
-    try{
-        clientSocket = serverSocket.accept();
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    Socket clientSocket;
+    ServerSocket serverSocket;
+
+    public Server() {
+        Ecoute();
     }
-    catch(IOException e){
-        System.out.println("Accept failed on port 4444");
-        System.exit(-1);
-    }
+    
+    public void Ecoute() {
+
+        try{
+            serverSocket = new ServerSocket(4444);
+        }
+        catch(IOException e){
+            System.out.println("Could not listen on port 4444");
+            System.exit(-1);
+        }
+
+        try{
+            clientSocket = serverSocket.accept();
+        }
+        catch(IOException e){
+            System.out.println("Accept failed on port 4444");
+            System.exit(-1);
+        }
+
+        System.out.println("Client connecté");
 
 }
 
-
-
-
+}
 
 
 
