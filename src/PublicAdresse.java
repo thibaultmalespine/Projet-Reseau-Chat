d5e96e374a9734d5a7a4daa8207eee3a7f5a7d7b
thibaultmalespine <thibault.malespine@orange.fr>
2024-11-08 12:17:04 +0100

    récupération de l'adresse public à finir

---
+++ src/PublicAdresse.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class PublicAdresse {
+    String adresse_public;
+    public PublicAdresse(){
+        URL url;
+        try {
+            url = new URL("ifconfig.me");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
+            for (String line; (line = reader.readLine()) != null;) {
+                adresse_public = line;
+            }
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        PublicAdresse pa = new PublicAdresse();
+        System.out.println(pa.adresse_public);
+    }
+}
